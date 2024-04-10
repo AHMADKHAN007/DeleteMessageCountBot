1038323f863ae80210642377174e3362fe3da2c5
Agent98 <Ahmad001khan2@gmail.com>
2024-04-10 15:56:38 +0330

    init bot

---
+++ BotConfig.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+session = "charge_wallet_bot"
+api_id = "1007132"
+api_hash = "16030ce60788367628f97288ebcadf06"
+bot_user_id = 6602013197
+userid = 5563379106
+bot = Client(
+    session,
+    api_id=api_id,
+    api_hash=api_hash,
+)
