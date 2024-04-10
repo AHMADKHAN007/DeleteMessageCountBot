32a4d41bc9b18d0213d73316d687454ff6ca27ec
Agent98 <Ahmad001khan2@gmail.com>
2024-04-10 16:36:11 +0330

    update

--- FanSubBot.py
+++ FanSubBot.py
@@ -27,15 +27,13 @@ counter = Counter()
 counter_del = Counter_del()
 
 
+
 @bot.on_message(filters.chat(userid) & filters.text & filters.regex('/del'))
 async def handle_check(client, message):
     limit = int(message.text.strip().split()[1])
-    async for mes in bot.get_chat_history(chat_id=bot_user_id, limit=limit):
-        if mes.text:
-            counter.increment()
-        else:
-            counter_del.increment()
-    await bot.send_message(chat_id=userid,
-                           text=f'count_del: {counter_del.get_count()}\nhas_text: {counter.get_count()}')
+    with open('del.txt', 'w', encoding='utf-8') as file:
+        async for mes in bot.get_chat_history(chat_id=bot_user_id, limit=limit):
+            file.write(f'id: {mes.id}\nDate: {mes.date}\n---------------\n')
+
 
 bot.run()
